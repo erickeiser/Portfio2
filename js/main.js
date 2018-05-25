e445d632b9cabc891ad52b919510881b33e7adfc
Eric Keiser <eric.keiser1@gmai.com>
2018-05-25 14:56:14 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var port = document.querySelectorAll('.port-item')
+var clicked = true 
+
+for(var i = 0; i < port.length; i++) {
+	
+}
+	
+
+
+
+
+
+ 
+
+
--- js/skill.js
+++ js/skill.js
@@ -1,23 +1,4 @@
-
-$('.port-item').click(function(){
-  $('.collapse').toggle('slow')
-})
-var clicked = false;
-$('#home').click(function(){
-  if(clicked) {
-     $(this).css('background-color', '#428bca')
-     $('.home').css('color', '#fff')
-     clicked = false;
-  } else {
-    $(this).css('background-color', '#333')
-    $('.home').css('color', '#428bca')
-    clicked = true;
-  }
-})
- 
-
-
-;( function( $ ) {
+( function( $ ) {
 	"use strict";
 	
 	var $bars = $( ".bar" ),
@@ -63,8 +44,3 @@ $('#home').click(function(){
 	methods.init();	
 		
 })( jQuery );
-
-
-
-
-
